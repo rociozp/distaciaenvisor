fc8366fd8b9b21bbc99f3a7c107d3361cc11d802
rociozp <99869550+rociozp@users.noreply.github.com>
2022-03-07 09:56:23 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-
+let distancia = 0
+basic.forever(function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
+    distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
+    if (distancia < 15) {
+        basic.showString("" + (distancia))
+        if (distancia < 6) {
+            basic.showString("" + (distancia))
+            maqueen.motorStop(maqueen.Motors.All)
+        }
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # . # . #
+            # . # . #
+            # # # # #
+            `)
+    }
+})
