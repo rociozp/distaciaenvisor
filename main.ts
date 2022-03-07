d24999f2ccc6f659dc88d3a1ed81017492282782
rociozp <99869550+rociozp@users.noreply.github.com>
2022-03-07 10:03:54 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,11 +2,11 @@ let distancia = 0
 basic.forever(function () {
     distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
     if (distancia < 15) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
         basic.showString("" + (distancia))
         if (distancia < 6) {
             basic.showString("" + (distancia))
             maqueen.motorStop(maqueen.Motors.All)
+            basic.pause(5000)
         }
     } else {
         basic.showLeds(`
