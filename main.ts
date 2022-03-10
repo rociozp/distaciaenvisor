fbb757a8ea71c5874b8087182f5d02246daaa68f
rociozp <99869550+rociozp@users.noreply.github.com>
2022-03-10 09:41:55 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,21 @@
 let distancia = 0
 basic.forever(function () {
     distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
-    if (distancia < 30) {
+    basic.showString("" + (distancia))
+    if (distancia < 60) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 5)
+        basic.pause(200)
+    }
+    if (distancia < 10) {
         basic.showString("" + (distancia))
-        if (distancia < 10) {
-            basic.showString("" + (distancia))
-            maqueen.motorStop(maqueen.Motors.All)
-            basic.pause(5000)
-        }
-    } else {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.pause(500)
         basic.showLeds(`
-            # # # # #
-            # # # # #
-            # . # . #
-            # . # . #
+            # # # . #
+            . . . # .
+            . . # . .
+            . # . . .
             # # # # #
             `)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
     }
 })
